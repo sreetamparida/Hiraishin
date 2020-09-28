b4c9a44207fa5d922eb61a76230f88c2d4b751ab
sreetamparida <sreetamparida123@gmail.com>
2020-09-29 01:17:07 +0530

    minor update

--- Elements/MapRed/MRSession.py
+++ Elements/MapRed/MRSession.py
@@ -11,7 +11,7 @@ class MRSession:
         initiate_command = self.config['initiate_command']
         os.system(initiate_command)
         cmd = 'hadoop jar {hadoop_streaming_jar} -file {element_path} -file {mapper_path} ' \
-              '-mapper mapper.py -file {reducer_path} -reducer reducer.py -input /{input_dir}/{table}.csv -output ' \
+              '-mapper mapper.py -file {reducer_path} -reducer reducer.py -input {input_dir}/{table}.csv -output ' \
               '{output_dir}'.format(
             hadoop_streaming_jar=self.config['hadoop_streaming_jar'],
             mapper_path=self.config['mapper_path'],
